60356352cc18fdea3767e7b41c73dcf64e3d2630
sergo2208 <baranov999m@mail.ru>
2021-11-16 22:53:14 +0300

    HomeWorks5

--- src/main/java/lesson3/HomeWork1.java
+++ src/main/java/lesson3/HomeWork1.java
@@ -4,20 +4,19 @@ public class HomeWork1 {
     public static void main(String[] args) {
         double a, b, c;
         double x1, x2;
-        a = 1;
-        b = 3;
-        c = 5;
-        double Discrimenant = b * b -4 * a * c;
-
-        if (Discrimenant == 0) {
+        a = 2;
+        b = 4;
+        c = 2;
+        double discrimenant = b * b -4 * a * c;
+        if (discrimenant == 0) {
             x1 = (-b) / (2 * a);
-            System.out.printf("x1 = x2 = %s", x1);
-        } else if (Discrimenant >  0) {
-            x1 = (-b + Math.sqrt(Discrimenant)) / (2 * a);
-            x2 = (-b - Math.sqrt(Discrimenant)) / (2 * a);
-            System.out.printf("x1 = %s, x2 = %s", x1, x2);
+            System.out.printf("x = %s", x1);
+        } else if (discrimenant >  0) {
+            x1 = (-b + Math.sqrt(discrimenant)) / (2 * a);
+            x2 = (-b - Math.sqrt(discrimenant)) / (2 * a);
+            System.out.printf("x1 = %s x2 = %s", x1, x2);
         } else {
-            System.out.println("Нет целого числа так как дискреминант < 0");
+            System.out.println("Нет действительных решений уравнения");
         }
     }
 }
--- src/main/java/lesson4/HomeWork4.java
+++ src/main/java/lesson4/HomeWork4.java
@@ -4,7 +4,6 @@ public class HomeWork4{
     public static void main(String[] args) {
         System.out.println("Квадратный корень числа n = " + squareRoot(64));
     }
-
     public static int squareRoot(int n) {
         int b = 1;
         while (b * b != n) {
