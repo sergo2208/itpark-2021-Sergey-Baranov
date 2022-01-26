7bf3ef64dd3e8c8b78af59d2a83a529401b17e5f
sergo2208 <baranov999m@mail.ru>
2022-01-26 18:02:47 +0300

    HomeWork20

---
+++ src/main/java/lesson20/Employee.java
@@ -0,0 +1,23 @@
+package lesson20;
+
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlAttribute;
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+@Data
+@Accessors(chain = true)
+@XmlAccessorType(XmlAccessType.FIELD)
+
+public class Employee {
+
+    @XmlAttribute
+    private String tabNumber;
+    @XmlAttribute
+    private String login;
+    private String fullName;
+    private Department department;
+    private Position position;
+
+}
